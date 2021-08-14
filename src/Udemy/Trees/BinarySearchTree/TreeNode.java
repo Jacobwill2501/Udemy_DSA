1dd92ec0871bb3cadb2cf2c7ea52f1286a23545c
Jacob <williams.jacob2501@gmail.com>
2021-08-14 13:50:22 -0400

    Add BST insertion

---
+++ src/Udemy/Trees/BinarySearchTree/TreeNode.java
@@ -0,0 +1,59 @@
+package Udemy.Trees.BinarySearchTree;
+
+
+public class TreeNode {
+
+    private int data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+    public void insert(int value) {
+        //to handle duplicate values, not allow them
+        if (value == data) {
+            return;
+        }
+
+        if (value < data) { //checking to see if we go down to the left
+            if (leftChild == null) { //if the node doesn't have a left subtree
+                leftChild = new TreeNode(value);
+            } else {
+                leftChild.insert(value); //call again to go down another level
+            }
+        } else { //then go down right subtree
+            if (rightChild == null) { //same as before but right subtree
+                rightChild = new TreeNode(value);
+            } else {
+                rightChild.insert(value);
+            }
+        }
+
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
